678a619e1647fa5f7dea4c5fd87dfad7afbb1597
lilian Robert <mslillianito@gmail.com>
2021-11-30 14:47:58 -0800

    one component two parts app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 const App = (props) => {
-    return(
-        <div className ="jumbotron">
-            <h1> {props.title} </h1>
-            <h3 > {props.header} </h3>
-            <p> {props.lead} </p>
-        </div>
-    );
+        return(
+            <div className="container">
+                <div className ="jumbotron">
+                    <h1> {props.title} </h1>
+                    <h3 > {props.header} </h3>
+                    <p> {props.lead} </p>
+                </div>
+                <div className="row" style= {{height: props.height}}>
+                    <div className="col" style={{background: props.color1}}>
+                        <h3 style={{color: props.color4}}> White on Blue </h3>
+                    </div>
+                    <div className="col" style={{background: props.color2}}>
+                        <h3 style={{color: props.color1}}> Blue on Red</h3>
+                    </div>
+                    <div className="col" style={{background: props.color3}}>
+                        <h3 style={{color: props.color5}}> Green on Pink </h3>
+                    </div>
+                </div>
+            </div>
+            
+        ); 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.js';
 
+
 const props = {
   title: "React JSX",
   header: "We made it!",
-  lead: " Learn more",
+  lead: "Learn more",
+  color1: "blue",
+  color2: "red",
+  color3: "pink",
+  color4: "white",
+  color5: "green",
+  height: "200px",
 };
 
 ReactDOM.render(
   App(props),
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // equivalent to:
-// ReactDOM.render(App(), document.getElementById("root"));
+// ReactDOM.render(<App title="React JSX" header="We made it!" lead ="Learn more" />, document.getElementById("root"));
